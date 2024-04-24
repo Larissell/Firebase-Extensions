ca987a320ef31c30e5a1295034247915557aaf1a
Larissa Souza <120027246+Larissell@users.noreply.github.com>
2024-04-23 21:48:55 -0300

    Update index.js
    js atualizado

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   measurementId: "G-S2VR1JKHPY"
   };
   
-<<<<<<< HEAD
 // Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage();
@@ -116,65 +115,6 @@ function checkIfImageHasLabels(imageUrl) {
 function displayLabels() {
   const imageLabelsRef = firestore.collection('imageLabels');
 
-=======
-  // Inicializa o Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  // Referência para o armazenamento e para o Firestore
-  const storage = firebase.storage();
-  const firestore = firebase.firestore();
-
-  // Chama a função para exibir os rótulos quando a página carregar
-  displayLabels();
-  
-  // Função para enviar a imagem para o Firebase Storage e salvar os dados no Firestore
-  function uploadImage() {
-    const fileInput = document.getElementById('image-upload');
-    const file = fileInput.files[0];
-  
-    if (!file) {
-      console.error('Nenhuma imagem selecionada.');
-      return;
-    }
-  
-    // Referência ao armazenamento da imagem
-    const imageRef = storage.ref(`/images/${file.name}`);
-  
-    // Realiza o upload da imagem
-    imageRef.put(file).then((snapshot) => {
-      console.log('Imagem enviada com sucesso para o Firebase Storage.');
-  
-      // Obtém a URL da imagem no armazenamento
-      snapshot.ref.getDownloadURL().then((imageUrl) => {
-        console.log('URL da imagem:', imageUrl);
-  
-        // Salva os dados no Firestore, com rótulos vazios inicialmente
-        firestore.collection('imageLabels').add({
-          file: imageUrl,
-          labels: [] // Inicialmente, os rótulos estão vazios
-        }).then((docRef) => {
-          console.log('Dados salvos no Firestore com o ID:', docRef.id);
-  
-          // Após o envio bem-sucedido da imagem, chama a função para exibir os rótulos
-          displayLabels();
-        }).catch((error) => {
-          console.error('Erro ao salvar dados no Firestore:', error);
-        });
-      }).catch((error) => {
-        console.error('Erro ao obter URL da imagem:', error);
-      });
-    }).catch((error) => {
-      console.error('Erro ao enviar imagem para o Firebase Storage:', error);
-    });
-  }
-
-// Função para exibir os rótulos das imagens na tela
-function displayLabels() {
-  // Referência para a coleção de imagens rotuladas no Firestore
-  const imageLabelsRef = firestore.collection('imageLabels');
-
-  // Consulta as imagens rotuladas
->>>>>>> 9253b7e8107fbb8e6af2b8c5e9c98c1ed77a5c4b
   imageLabelsRef.get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
       const imageData = doc.data();
@@ -185,7 +125,6 @@ function displayLabels() {
 
       // Adiciona a imagem ao elemento div
       const imageElement = document.createElement('img');
-<<<<<<< HEAD
       imageElement.alt = 'Imagem';
       imageElement.style.maxWidth = '80%'; // Define a largura máxima como 100%
       imageElement.style.maxHeight = '160px'; // Define a altura máxima como 300px
@@ -211,26 +150,6 @@ function displayLabels() {
 
       // Adiciona o elemento div ao contêiner de rótulos
       document.getElementById('labelsContainer').appendChild(imageDiv);
-=======
-      storage.refFromURL(imageData.file).getDownloadURL().then((imageUrl) => {
-        imageElement.src = imageUrl;
-        // Estilize a altura e a largura da imagem aqui
-        imageElement.style.maxWidth = '80%'; // Define a largura máxima como 100%
-        imageElement.style.maxHeight = '160px'; // Define a altura máxima como 300px
-      }).catch((error) => {
-        console.error('Erro ao obter URL da imagem no Firebase Storage:', error);
-      });
-      imageElement.alt = 'Imagem';
-      imageDiv.appendChild(imageElement);
-
-      // Adiciona os rótulos da imagem ao elemento div
-      const labelsElement = document.createElement('p');
-      labelsElement.textContent = `Rótulos da imagem: ${imageData.labels.join(', ')}`;
-      imageDiv.appendChild(labelsElement);
-
-      // Adiciona o elemento div ao contêiner de rótulos
-      labelsContainer.appendChild(imageDiv);
->>>>>>> 9253b7e8107fbb8e6af2b8c5e9c98c1ed77a5c4b
     });
   }).catch((error) => {
     console.error('Erro ao obter imagens rotuladas do Firestore:', error);
